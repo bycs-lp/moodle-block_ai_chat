fffd1e2e486efd2fa4273f7a0df509e0b66f4adb
Philipp Memmel <philipp.memmel@gmail.com>
2025-03-05 16:31:46 +0100

    MBS-9974: Fixes for moodle 5

--- amd/src/helper.js
+++ amd/src/helper.js
@@ -13,8 +13,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-import {typeset} from 'filter_mathjaxloader/loader';
-
 /**
  * Copy ai reply to clipboard.
  * @param {*} element
