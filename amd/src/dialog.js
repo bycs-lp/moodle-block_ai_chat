e37f2f253f062eb5ab662aec8020a43cbf6a5725
Philipp Memmel <philipp.memmel@gmail.com>
2025-08-21 22:51:02 +0200

    MBS-10249: Fix error if no persona systemtemplates are available

--- amd/src/dialog.js
+++ amd/src/dialog.js
@@ -1056,9 +1056,14 @@ const showPersonasModal = () => {
             select.insertBefore(systemtemplates, select.options[2]);
             // // Add usertemplates heading.
             if (useroptions) {
-                // Get last systemtemplate position.
-                const maxValue = Math.max(...templateids.map(id => parseInt(id)));
-                const lastSystemOption = Array.from(select.options).find(opt => parseInt(opt.value) === maxValue);
+                let lastSystemOption;
+                if (templateids.length === 0) {
+                    lastSystemOption = systemtemplates;
+                } else {
+                    // Get last systemtemplate position.
+                    const maxValue = Math.max(...templateids.map(id => parseInt(id)));
+                    lastSystemOption = Array.from(select.options).find(opt => parseInt(opt.value) === maxValue);
+                }
                 // Add heading.
                 const usertemplates = new Option(strUserTemplates, '', false, false);
                 usertemplates.disabled = true;
