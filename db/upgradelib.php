5ec2fca5852e314403af0c5a495699951673e2f7
Philipp Memmel <philipp.memmel@mailbox.org>
2026-01-23 15:22:48 +0100

    MBS-10478: Fix codestyle

--- db/upgradelib.php
+++ db/upgradelib.php
@@ -23,8 +23,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 
 /**
  * It has been overseen that some data is not being removed when deleting block instances.
