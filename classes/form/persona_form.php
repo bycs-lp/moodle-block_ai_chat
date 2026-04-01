73f935c7b965123a0aa9930effd2a80da4a18de5
Philipp Memmel <philipp.memmel@mailbox.org>
2026-04-01 10:14:58 +0200

    MBS-10643: phpcs

--- classes/form/persona_form.php
+++ classes/form/persona_form.php
@@ -144,8 +144,8 @@ class persona_form extends dynamic_form {
         if (empty(trim(strip_tags($data['prompt']['text'])))) {
             $errors['prompt'] = get_string('errorprompt', 'block_ai_chat');
         }
-        $hasDataUrl = (bool) preg_match('/(src|href)\s*=\s*["\']?\s*data\s*:/i', $data['prompt']['text']);
-        if ($hasDataUrl) {
+        $hasdataurl = (bool) preg_match('/(src|href)\s*=\s*["\']?\s*data\s*:/i', $data['prompt']['text']);
+        if ($hasdataurl) {
             $errors['prompt'] = get_string('errorpromptdataurl', 'block_ai_chat');
         }
         if (empty($data['userinfo'])) {
